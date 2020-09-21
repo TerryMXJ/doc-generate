df9c142cc5752eaf5ac68f1a15d96de1c75a63ff
Terry Meng <mxj2478430@qq.com>
2020-09-21 21:52:17 +0800

    modified add related api script

--- script/add_related_api_to_graph.py
+++ script/add_related_api_to_graph.py
@@ -13,17 +13,6 @@ graph_data_save_path = PathUtil.graph_data(pro_name=pro_name, version="v3.10")
 
 graph_data: GraphData = GraphData.load(graph_data_path)
 
-# def find_adj_node_with_label(id, label):
-#     result = []
-#     for i in graph_data.get_all_in_relations(id):
-#         if label in graph_data.find_nodes_by_ids(i[0])[0]["labels"]:
-#             result.append(i[0])
-#     for i in graph_data.get_all_in_relations(id):
-#         if label in graph_data.find_nodes_by_ids(i[2])[0]["labels"]:
-#             result.append(i[2])
-#     return result
-
-
 def create_subgraph(G, sub_G, start_point, step):
     if step > 2:
         return
@@ -42,11 +31,13 @@ if __name__ == '__main__':
     kinds = ['class', 'method']
     G = nx.Graph(graph_data.graph)
     for kind in kinds:
+        num = 0
         print("*************************" + kind + "***********************")
         nodes = graph_data.get_node_ids_by_label(kind)
         for i in nodes:
+            num += 1
             origin_node: GraphData = graph_data.find_nodes_by_ids(i)[0]
-            print("start process {}".format(origin_node['properties']['qualified_name']))
+            print("{}: start process {}".format(num, origin_node['properties']['qualified_name']))
             sub_G = nx.Graph()
             create_subgraph(G, sub_G, i, 1)
             sim_result = simrank_cal(sub_G, i)
@@ -57,7 +48,7 @@ if __name__ == '__main__':
                 if count > 5: break
                 node: NodeInfo = graph_data.find_nodes_by_ids(j[0])[0]
                 if kind in node["labels"]:
-                    result.append(j[0])
+                    result.append(j[0]['properties']['qualified_name'])
                     count += 1
             origin_node['properties']['simrank'] = result
             print("save simrank result: " + str(result))
